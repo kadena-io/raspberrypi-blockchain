a90095eac5be4016ade49651cc9212e44e61aa5d
cobanmus <mustafa.coban@bigdatateknoloji.com>
2019-05-28 17:59:10 +0300

    pact and api code updated for reading gps

--- logsensordata.py
+++ logsensordata.py
@@ -1,5 +1,8 @@
 import sys
 import time
+import threading
+import pynmea2
+import os
 from pprint import pprint
 from datetime import datetime
 
@@ -9,7 +12,7 @@ from pypact import api
 from pypact.adapters import BasePactAdapter
 
 
-sample_freq = 5  # 20 minutes in seconds
+sample_freq = 10  # 20 minutes in seconds
 sensor = Adafruit_DHT.DHT22
 pin = 16
 
@@ -26,13 +29,14 @@ def read_sensor_data():
     return "Failed to read sensor data!"
 
 
-def send_sensor_data(temp, humidity):
+def send_sensor_data(temp, humidity, latitude, longitude):
     """Sends data to pact server to be saved on blockchain"""
     code = BasePactAdapter.build_code(
         "raspberrypi",
-        "update-temp-humidity",
+        "update-temp-humidity-gps",
         "admin-keyset",
         **{"temp": temp, "humidity": humidity,
+           "latitude": latitude, "longitude": longitude,
            "keyset_name": "admin-keyset",
            "time": format_current_time()}
     )
@@ -51,11 +55,78 @@ def print_log():
     result = api.send_and_listen(code, "admin-keyset")
     pprint(result, indent=2)
 
+class Message:
+    def __init__(self):
+        self.msg =''
+
+# Gps Receiver thread funcion, check gps value for infinte times
+def getgpsdata(serial, dmesg):
+    print("getgpsdata")
+    while True:
+        data = serial.readline()
+        if data.find('GGA') > 0:
+            dmesg.msg = pynmea2.parse(data)
+
+# API to call start the GPS Receiver
+def start_gps_receiver(serial, dmesg):
+    t2 = threading.Thread(target=getgpsdata, args=(serial, dmesg))
+    t2.start()
+    print("GPS Receiver started")
+
+# API to fix the GPS Revceiver
+def ready_gps_receiver(msg):
+    print("Please wait fixing GPS .....")
+    dmesg = msg.msg
+    while(dmesg.gps_qual != 1):
+        pass
+    print("GPS Fix available")
+
+# API to get latitude from the GPS Receiver
+def get_latitude(msg):
+    print("Getting Latitude")
+    dmesg = msg.msg
+    print("Latitude:", dmesg.latitude)
+
+# API to get longitude from the GPS Receiver
+def get_longitude(msg):
+    print("Getting Longitude")
+    dmesg = msg.msg
+    print("Longitude:", dmesg.longitude)
+
+# API to get Number of Satellites from the GPS Receiver
+def get_num_satellite(msg):
+    print("Getting Number of satellite")
+    dmesg = msg.msg
+    print("No of satellites:", dmesg.num_sats)
+
+# API to get Altitude of Antenna from the GPS Receiver
+def get_altitude(msg):
+    print("Getting altitude of Antenna")
+    dmesg = msg.msg
+    print("Altitude (M):", dmesg.altitude)
+
+# API to check the status of GPS Fix
+def get_gps_fix(msg):
+    dmesg = msg.msg
+    print("GPS Fix stats:", dmesg.gps_qual)
+
+# API to Exit
+def function_exit(msg):
+    print("Exiting ......")
+    print("stopping the thread")
+    exit()
+    return 1
+msgdata = Message() # Creates a Message Instance
 
 def main():
     while True:
         temp, humidity = read_sensor_data()
-        send_sensor_data(temp, humidity)
+        start_gps_receiver(ser, msgdata)
+        print(msgdata.msg)
+        ready_gps_receiver(msgdata)
+        latitude = get_latitude(msgdata.msg)
+        longitude = get_longitude(msgdata.msg)
+        send_sensor_data(temp, humidity, latitude, longitude)
         time.sleep(sample_freq)
 
 
@@ -66,4 +137,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\n", "Stopping script...")
         sys.exit()
-
--- pact/contracts/raspberrypi.pact
+++ pact/contracts/raspberrypi.pact
@@ -1,47 +1,58 @@
 (define-keyset 'admin-keyset (read-keyset "admin-keyset"))
 
 (module raspberrypi 'admin-keyset
- @doc "Read and Update temperature and humidity."
+ @doc "Read and Update temperature, humidity and gps."
 
- (defschema temp-humidity
-  @doc "One row table for temeperature and humidity"
+ (defschema temp-humidity-gps
+  @doc "One row table for temeperature, humidity and gps"
   temperature:decimal
   humidity:decimal
+  latitude:decimal
+  longitude:decimal
   time:time
   keyset:keyset)
 
- (deftable temp-humidity-table:{temp-humidity})
+ (deftable temp-humidity-gps-table:{temp-humidity-gps})
 
- (defun update-temp-humidity ( temperature:decimal
-                               humidity:decimal
-                               keyset:string
-                               time:time )
-  @doc "updates temperature and humidity values for every 20 minutes"
+ (defun update-temp-humidity-gps ( temperature:decimal
+                                   humidity:decimal
+                                   latitude:decimal
+                                   longitude:decimal
+                                   time:time
+                                   keyset:string )
+  @doc "updates temperature, humidity and gps values for every 20 minutes"
   (enforce-keyset (read-keyset "admin-keyset"))
-  (write temp-humidity-table "1"
+  (write temp-humidity-gps-table "1"
          { "temperature": temperature
          , "humidity": humidity
+         , "latitude": latitude
+         , "longitude": longitude
          , "time" : time
          , "keyset": (read-keyset keyset)
          }))
 
- (defun read-temp-humidity ()
+ (defun read-temp-humidity-gps ()
   @doc "Read only row from table and returns it."
-  (with-read temp-humidity-table "1"
+  (with-read temp-humidity-gps-table "1"
              { "temperature":=temperature
              , "humidity":=humidity
+             , "latitude":= latitude
+             , "longitude":= longitude
              , "time":=time
-             } [temperature humidity time]))
+             } [temperature humidity latitude longitude time]))
 
  (defun logs()
   @doc "Gives all updates"
-  (keylog temp-humidity-table "1" 0))
+  (keylog temp-humidity-gps-table "1" 0))
 )
 
-(create-table temp-humidity-table)
-(insert temp-humidity-table "1"
+(create-table temp-humidity-gps-table)
+
+(insert temp-humidity-gps-table "1"
         { "temperature": 25.1
         , "humidity": 70.5
+        , "latitude": 10.123
+        , "longitude": 32.55556
         , "time" : (time "2016-07-23T13:30:45Z")
         , "keyset": (read-keyset "admin-keyset")
         })
